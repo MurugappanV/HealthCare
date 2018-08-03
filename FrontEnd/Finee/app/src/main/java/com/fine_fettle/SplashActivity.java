1971eba425ebffc742a4a36f62addf108fe65399
Murugappan <murugappanviswanathan@gmail.com>
2018-08-04 02:02:02 +0530

    Main app - automatic login

--- FrontEnd/Finee/app/src/main/java/com/fine_fettle/SplashActivity.java
+++ FrontEnd/Finee/app/src/main/java/com/fine_fettle/SplashActivity.java
@@ -5,8 +5,10 @@ package com.fine_fettle;
  */
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -17,18 +19,35 @@ public class SplashActivity extends AppCompatActivity {
 
         Thread timerThread = new Thread(){
             public void run(){
-                try{
-                    sleep(2000);
-                }catch(InterruptedException e){
-                    e.printStackTrace();
-                }finally{
-                    Intent intent = new Intent(SplashActivity.this,MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
+            try{
+                sleep(2000);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }finally{
+                callBack();
+            }
             }
         };
         timerThread.start();
+    }
+
+    private void callBack() {
+        Intent intent;
+        SharedPreferences prefs = getSharedPreferences("LoginInfo", MODE_PRIVATE);
+        int id = prefs.getInt("id", -1);
+        if (id != -1) {
+            String name = prefs.getString("name", "User");
+            String pass = prefs.getString("pass", "pass");
+            //Toast.makeText(getApplicationContext(), "Welcome" + name, Toast.LENGTH_LONG).show();
+            intent = new Intent(SplashActivity.this, Personhome.class);
+            intent.putExtra("id",id+"");
+            intent.putExtra("user", name);
+            intent.putExtra("pass", pass);
+        } else {
+            intent = new Intent(SplashActivity.this,MainActivity.class);
+        }
+        startActivity(intent);
+        finish();
     }
 
 }
