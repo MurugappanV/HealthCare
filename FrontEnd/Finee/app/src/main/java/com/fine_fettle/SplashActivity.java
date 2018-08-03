8d8db971d8e2ea0710bc613c4cebdeac1a8b4648
Murugappan <murugappanviswanathan@gmail.com>
2018-08-03 23:56:26 +0530

    Main app - splash screen time change

--- FrontEnd/Finee/app/src/main/java/com/fine_fettle/SplashActivity.java
+++ FrontEnd/Finee/app/src/main/java/com/fine_fettle/SplashActivity.java
@@ -18,7 +18,7 @@ public class SplashActivity extends AppCompatActivity {
         Thread timerThread = new Thread(){
             public void run(){
                 try{
-                    sleep(5000);
+                    sleep(2000);
                 }catch(InterruptedException e){
                     e.printStackTrace();
                 }finally{
