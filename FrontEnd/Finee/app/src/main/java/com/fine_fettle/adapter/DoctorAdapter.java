272913281b7beddcc8fcb12a4b6704ed2375a513
Murugappan <murugappanviswanathan@gmail.com>
2018-08-06 02:08:59 +0530

    Main app - hospital and doctor adapter

--- FrontEnd/Finee/app/src/main/java/com/fine_fettle/adapter/DoctorAdapter.java
+++ FrontEnd/Finee/app/src/main/java/com/fine_fettle/adapter/DoctorAdapter.java
@@ -1,8 +1,96 @@
 package com.fine_fettle.adapter;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.fine_fettle.Laboratory;
+import com.fine_fettle.MakeAppointment;
+import com.fine_fettle.R;
+import com.fine_fettle.models.DoctorModal;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
 /**
  * Created by murugappanviswanathan on 06/08/18.
  */
 
-public class DoctorAdapter {
+public class DoctorAdapter  extends RecyclerView.Adapter<DoctorAdapter.DoctorViewHolder> {
+    ArrayList<DoctorModal> mTipsList;
+    Context mContext;
+    private LayoutInflater inflater;
+
+
+    public DoctorAdapter(Context context, int resource, ArrayList<DoctorModal> tipsList) {
+        mTipsList = tipsList;
+        mContext = context;
+        if (mContext != null) {
+            inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+    }
+
+    @NonNull
+    @Override
+    public DoctorAdapter.DoctorViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.doctor_item_layout, parent, false);
+        DoctorAdapter.DoctorViewHolder holder = new DoctorAdapter.DoctorViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DoctorAdapter.DoctorViewHolder holder, int position) {
+        final DoctorModal tip = mTipsList.get(position);
+
+        Picasso.get()
+                .load(tip.getGender().equals("male") ? R.drawable.male_doctor : R.drawable.female_doctor)
+                .placeholder(R.drawable.male_doctor)
+                .error(R.drawable.male_doctor)
+                .into(holder.mIcon);
+
+        holder.mTitle.setText(tip.getName() + ", " + tip.getHospital_name());
+        holder.mDescription.setText(tip.getSpecialization());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(mContext,Laboratory.class);
+//                intent.putExtra("id",  ((MakeAppointment)mContext).id);
+//                intent.putExtra("hospital_name", tip.getHospital_name() );
+//                mContext.startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mTipsList.size()>0) {
+            return mTipsList.size();
+        }else{
+            return 0;
+        }
+    }
+
+
+    public class DoctorViewHolder extends RecyclerView.ViewHolder {
+        private TextView mTitle;
+        private TextView mDescription;
+        private ImageView mIcon;
+
+
+        public DoctorViewHolder(View itemView) {
+            super(itemView);
+            mTitle = (TextView) itemView.findViewById(R.id.name);
+            mDescription = (TextView) itemView.findViewById(R.id.specialisation);
+            mIcon = (ImageView) itemView.findViewById(R.id.doc_icon);
+
+        }
+
+    }
 }
--- FrontEnd/Finee/app/src/main/java/com/fine_fettle/adapter/HospitalAdapter.java
+++ FrontEnd/Finee/app/src/main/java/com/fine_fettle/adapter/HospitalAdapter.java
@@ -1,8 +1,98 @@
 package com.fine_fettle.adapter;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.fine_fettle.Laboratory;
+import com.fine_fettle.MakeAppointment;
+import com.fine_fettle.R;
+import com.fine_fettle.TipsDetailActivity;
+import com.fine_fettle.models.HospitalModel;
+import com.fine_fettle.models.TipsModel;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
 /**
  * Created by murugappanviswanathan on 05/08/18.
  */
 
-public class HospitalAdapter {
+public class HospitalAdapter  extends RecyclerView.Adapter<HospitalAdapter.HospitalViewHolder> {
+    ArrayList<HospitalModel> mTipsList;
+    Context mContext;
+    private LayoutInflater inflater;
+
+
+    public HospitalAdapter(Context context, int resource, ArrayList<HospitalModel> tipsList) {
+        mTipsList = tipsList;
+        mContext = context;
+        if (mContext != null) {
+            inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+    }
+
+    @NonNull
+    @Override
+    public HospitalAdapter.HospitalViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.hospital_item_layout, parent, false);
+        HospitalAdapter.HospitalViewHolder holder = new HospitalAdapter.HospitalViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HospitalAdapter.HospitalViewHolder holder, int position) {
+        final HospitalModel tip = mTipsList.get(position);
+
+        Picasso.get()
+                .load(R.drawable.hospital_icon)
+                .placeholder(R.drawable.hospital_icon)
+                .error(R.drawable.hospital_icon)
+                .into(holder.mIcon);
+
+        holder.mTitle.setText(tip.getHospital_name() + ", " + tip.getHospital_city());
+        holder.mDescription.setText(tip.getHospital_address() + ", " + tip.getHospital_pincode());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext,Laboratory.class);
+                intent.putExtra("id",  ((MakeAppointment)mContext).id);
+                intent.putExtra("hospital_name", tip.getHospital_name() );
+                mContext.startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mTipsList.size()>0) {
+            return mTipsList.size();
+        }else{
+            return 0;
+        }
+    }
+
+
+    public class HospitalViewHolder extends RecyclerView.ViewHolder {
+        private TextView mTitle;
+        private TextView mDescription;
+        private ImageView mIcon;
+
+
+        public HospitalViewHolder(View itemView) {
+            super(itemView);
+            mTitle = (TextView) itemView.findViewById(R.id.name);
+            mDescription = (TextView) itemView.findViewById(R.id.address);
+            mIcon = (ImageView) itemView.findViewById(R.id.hosp_icon);
+
+        }
+
+    }
 }
