bc559135a08afd6b4090bc7b4ffc3f3785549ae1
Murugappan <murugappanviswanathan@gmail.com>
2018-08-05 23:08:37 +0530

    Main app - view appointment

--- FrontEnd/Finee/app/src/main/java/com/fine_fettle/adapter/ViewAppoinmentAdapter.java
+++ FrontEnd/Finee/app/src/main/java/com/fine_fettle/adapter/ViewAppoinmentAdapter.java
@@ -1,8 +1,99 @@
 package com.fine_fettle.adapter;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.fine_fettle.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by murugappanviswanathan on 05/08/18.
  */
 
-public class ViewAppoinmentAdapter {
+
+public class ViewAppoinmentAdapter  extends RecyclerView.Adapter<ViewAppoinmentAdapter.AppointmentViewHolder> {
+
+    ArrayList<HashMap<String, String>> mAppointmnetList;
+    Context mContext;
+    private LayoutInflater inflater;
+
+
+    public ViewAppoinmentAdapter(Context context, int resource, ArrayList<HashMap<String, String>> tipsList) {
+        mAppointmnetList = tipsList;
+        mContext = context;
+        if (mContext != null) {
+            inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+    }
+
+    @NonNull
+    @Override
+    public AppointmentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.appointment_item_layout, parent, false);
+        AppointmentViewHolder holder = new AppointmentViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AppointmentViewHolder holder, int position) {
+        final HashMap<String, String> tip = mAppointmnetList.get(position);
+
+
+        holder.mPatient.setText("Patient : " + tip.get("p_name"));
+        holder.mAppNo.setText("App.no : FFA0000" + tip.get("id"));
+        holder.mIssue.setText("Issue    : " + tip.get("h_issue"));
+        holder.mDocHos.setText(tip.get("doc_name") +", "+ tip.get("hos_name").toUpperCase());
+        holder.mDate.setText(tip.get("date").substring(8,10) + "-" +tip.get("date").substring(5,7) + "-" + tip.get("date").substring(0,4) );
+        holder.mSlot.setText(tip.get("slot"));
+
+//        holder.itemView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(mContext,TipsDetailActivity.class);
+//                intent.putExtra("tip_model",  tip);
+//                mContext.startActivity(intent);
+//            }
+//        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mAppointmnetList.size()>0) {
+            return mAppointmnetList.size();
+        }else{
+            return 0;
+        }
+    }
+
+
+    public class AppointmentViewHolder extends RecyclerView.ViewHolder {
+        private TextView mPatient;
+        private TextView mAppNo;
+        private TextView mIssue;
+        private TextView mDate;
+        private TextView mDocHos;
+        private TextView mSlot;
+
+
+        public AppointmentViewHolder(View itemView) {
+            super(itemView);
+            mPatient = (TextView) itemView.findViewById(R.id.patient);
+            mAppNo = (TextView) itemView.findViewById(R.id.appoinmntNo);
+            mIssue = (TextView) itemView.findViewById(R.id.issue);
+            mDate = (TextView) itemView.findViewById(R.id.date);
+            mDocHos = (TextView) itemView.findViewById(R.id.docHos);
+            mSlot = (TextView) itemView.findViewById(R.id.time);
+
+
+        }
+
+    }
 }
