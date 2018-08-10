6269a3ff961a488947fa3bfb62198dc70057cfa2
Murugappan <murugappanviswanathan@gmail.com>
2018-08-10 10:45:18 +0530

    Main app - ambulance files

--- FrontEnd/Finee/app/src/main/java/com/fine_fettle/latlng.java
+++ FrontEnd/Finee/app/src/main/java/com/fine_fettle/latlng.java
@@ -4,5 +4,49 @@ package com.fine_fettle;
  * Created by murugappanviswanathan on 10/08/18.
  */
 
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by 9DOTS04 on 3/1/2017.
+ */
+
 public class latlng {
+    @SerializedName("status")
+    private int status_code;
+    @SerializedName("address")
+    private String address;
+    @SerializedName("latitude")
+    private String lat;
+    @SerializedName("longitude")
+    private String lng;
+
+    public int getStatus_code() {
+        return status_code;
+    }
+
+    public void setStatus_code(int status_code) {
+        this.status_code = status_code;
+    }
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
 }
+
--- FrontEnd/Finee/app/src/main/java/com/fine_fettle/latlngapi.java
+++ FrontEnd/Finee/app/src/main/java/com/fine_fettle/latlngapi.java
@@ -4,5 +4,18 @@ package com.fine_fettle;
  * Created by murugappanviswanathan on 10/08/18.
  */
 
-public class latlngapi {
+import retrofit.Callback;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
+import retrofit.http.POST;
+
+/**
+ * Created by 9DOTS04 on 3/1/2017.
+ */
+
+public interface latlngapi {
+    @FormUrlEncoded
+    @POST("/get_latlng")
+    public void getforward(@Field("device") String device,
+                           Callback<latlng> response);
 }
